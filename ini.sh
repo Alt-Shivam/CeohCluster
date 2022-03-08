3f1f777c2122e403caa235d04ff908b695bd812e
Shivank Chaudhary <81817735+Alt-Shivam@users.noreply.github.com>
2022-03-08 23:58:51 +0530

    Create ini.sh

---
+++ ini.sh
@@ -0,0 +1,29 @@
+git clone --single-branch --branch v1.8.6 https://github.com/rook/rook.git
+
+cd CeohCluster/
+
+
+cd rook/deploy/examples
+
+
+rm -R cluster.yaml
+
+kubectl create -f crds.yaml -f common.yaml -f operator.yaml
+
+
+
+sleep 300
+
+
+kubectl create -f ../../../cluster.yaml
+
+
+sleep 300
+
+cd csi/rbd/
+
+kubectl create -f storageclass.yaml
+
+
+
+echo "please run ...........   kubectl exec -it rook-ceph-tools-55ddbc9f78-2dg9b -n rook-ceph ceph status"
